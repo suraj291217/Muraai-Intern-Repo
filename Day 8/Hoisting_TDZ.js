34c8870db940e5319569eb76353efe61b2b57dc5
Suraj Jagadeesh <surajjagadeesh1@gmail.com>
2026-09-07 18:20:08 +0530

    day8 task- Event Loop Hoisting TDZ

---
+++ Day 8/Hoisting_TDZ.js
@@ -0,0 +1,23 @@
+/*
+// LET AND CONST BEHAVIOUR(Hoisting)
+console.log(x);
+let x = 10;
+
+// VAR BEHAVIOUR(Hoisting)
+console.log(x);
+var x = 10;
+console.log(x);
+
+//-----------------------------------------------------------
+
+// LET AND CONST BEHAVIOUR(TDZ)
+console.log(myNumber);
+let myNumber = 10;
+console.log(myNumber);
+
+
+// VAR BEHAVIOUR(TDZ)
+console.log(oldVariable);
+var oldVariable = 11;
+console.log(oldVariable);
+*/
---
+++ Day 8/UtilityFunctions.js
@@ -0,0 +1,10 @@
+// Date
+// let date = new Date("2026-09-07");
+// console.log(date.toLocaleString);
+
+// getFullYear()
+let date = new Date();
+// console.log(date.getFullYear());
+
+// getMonth()
+console.log(date.getMonth());
